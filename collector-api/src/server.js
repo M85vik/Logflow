7861ea0a7f5a1e5b3c92d341293a4df35b4d6ee3
Vikas Sharma <vikas@Vikass-MacBook-Air.local>
2026-03-11 21:59:19 +0530

    start script added

--- collector-api/src/server.js
+++ collector-api/src/server.js
@@ -4,13 +4,13 @@ dotenv.config();
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
-import { redis } from "./config/redis.js"
+
 import logRoutes from "./routes/logs.js"
 import projectRoutes from "./routes/project.js"
 connectDB()
-redis.ping().then(console.log); 
-const app = express()
 
+const app = express()
+const PORT = process.env.PORT;
 app.use(cors())
 app.use(express.json())
 
@@ -24,7 +24,7 @@ app.get("/health", (req,res)=>{
 app.use("/logs", logRoutes)
 app.use("/projects", projectRoutes)
 
-app.listen(4000, ()=>{
-    console.log("Logger Server running on port 4000");
+app.listen(PORT, ()=>{
+    console.log(`Logger Server running on port ${PORT}`);
     
 })
